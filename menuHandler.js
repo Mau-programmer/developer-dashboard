8ff952d6be3feee2a0808107686323909a948475
Maurits the Programmer <maugrammer@outlook.com>
2026-02-27 18:37:12 +0100

    Add about section toggle functionality

--- menuHandler.js
+++ menuHandler.js
@@ -1,10 +1,18 @@
 const todosCon = document.getElementById('todosContainer');
 const goal = document.getElementById('goalContainer');
+const about = document.getElementById("aboutContainer");
 
 function openTodos() {
     todosCon.classList.add('show');
     goal.classList.remove('show');
+    about.classList.remove('show');
+
 } function openGoal() {
     todosCon.classList.remove('show');
     goal.classList.add('show');
+    about.classList.remove('show')
+} function openAbout() {
+    todosCon.classList.remove('show');
+    goal.classList.remove('show');
+    about.classList.add('show')
 }
